25a0120f9c173d3f98539f2ccfe3796ceed181ce
AlessandroScarlatti02 <alessandro.scarlatti2002@gmail.com>
2024-12-01 12:10:01 +0100

    Add Models and Repos

---
+++ src/main/java/it/lessons/pizzeria/repository/IngredientRepository.java
@@ -0,0 +1,13 @@
+package it.lessons.pizzeria.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.lessons.pizzeria.model.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
+
+    public List<Ingredient> findByName(String name);
+
+}
